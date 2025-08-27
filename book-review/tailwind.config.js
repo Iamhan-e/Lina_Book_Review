9fc2bcd427b089f19c32ca57736b626178975de6
Iamhan-e <h2anaschalew@gmail.com>
2025-08-28 00:11:08 +0300

    changed color and layout

--- book-review/tailwind.config.js
+++ book-review/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellowSoft: "#ffe66d",
+        redCoral: "#ff6b6b",
+        turquoise: "#4ecdc4",
+        offWhite: "#f7fff7",
+        tealDark: "#1a535c",
+      },
+    },
   },
   plugins: [],
 }
-
