60cc7e34a50175ef5b1cbebfb43bdae1ea657ff8
Iamhan-e <h2anaschalew@gmail.com>
2025-08-29 22:35:41 +0300

    chore: configure tailwind colors and typography

--- book-review/tailwind.config.js
+++ book-review/tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -13,7 +13,21 @@ module.exports = {
         offWhite: "#f7fff7",
         tealDark: "#1a535c",
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.tealDark"),
+            a: {
+              color: theme("colors.redCoral"),
+              textDecoration: "none",
+            },
+            h1: { color: theme("colors.tealDark") },
+            h2: { color: theme("colors.tealDark") },
+            "blockquote p": { color: theme("colors.tealDark") },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 }
